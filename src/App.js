e7ae611ad020fc68def904aac7d3c81d7370b52a
sachinprasad3 <sachinprasad841@gmail.com>
2021-12-03 11:36:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Add from './pages/Add';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import View from './pages/View';
+import Navbar from './components/Navbar';
+import Update from './pages/Update';
+import Home from './pages/Home';
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" exact component={Add}/> */}
+          {/* <Route path="/view" component={View}/> */}
+
+          <Route path="/" element={<Home />}/>
+          <Route path="/view" element={<View />}/>
+          <Route path="/add" element={<Add />}/>
+          <Route path="/update/:id" element={<Update />}/>
+
+        </Routes>
+      </Router>
+
+      <ToastContainer />
+
     </div>
   );
 }
